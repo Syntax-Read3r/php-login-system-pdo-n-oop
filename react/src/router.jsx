ff054698c34815298d07118e73657e2677744f5e
Munyaradzi <syntaxread3r@gmail.com>
2023-02-25 20:21:48 +0000

    fix router.jsx as it had incorrect code

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,9 +1,28 @@
-import {createBrowserRouter} from 'react-router-dom';
+import { createBrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Users from './pages/Users';
+import SignUp from "./pages/SignUp";
+import NotFound from "./pages/NotFound";
 
-const router = createBrowserRouter({
-  routes: [
-    // your routes go here 
-  ]
-});
+const routes = [
+    {
+        path: "/login",
+        element: <Login />,
+    },
+    {
+        path: "/users",
+        element: <Users />,
+    },
+    {
+        path: "/signup",
+        element: <SignUp />,
+    },
+    {
+        path: '*',
+        element: <NotFound/>
+    }
+];
+
+const router = createBrowserRouter(routes);
 
 export default router;
