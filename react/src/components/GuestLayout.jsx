5c14593c210810caf401beb247801d10ffe35d3c
Munyaradzi <syntaxread3r@gmail.com>
2023-02-25 22:01:41 +0000

    add out outlet in GuestLayout.jsx

--- react/src/components/GuestLayout.jsx
+++ react/src/components/GuestLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function GuestLayout() {
   return (
     <div>
-        
+        For guest users only
+        <Outlet/>
     </div>
   )
 }
