3469de1519caed48f4fc354ce3c67ef2363028aa
Munyaradzi <syntaxread3r@gmail.com>
2023-02-26 16:17:07 +0000

    add logic to Signup.jsx

--- react/src/pages/SignUp.jsx
+++ react/src/pages/SignUp.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 export default function SignUp() {
+
+const onSubmit = (e) => {
+  return e.preventDefault();
+}
+
+
   return (
-    <div>SignUp</div>
+    <div className="login-signup-form animated fadeInDown">
+            <div className="form">
+                <form onSubmit={onSubmit}>
+                  <h1 className="title">Signup for free</h1>
+                    <input type="text" placeholder="Full Name" />
+                    <input type="email" placeholder="Email Address" />
+                    <input type="password" placeholder="Password" />
+                    <input type="password" placeholder="Password Confirmation" />
+                    <button className="btn btn-block">Signup</button>
+                    <p className="message">
+                      Already Regestired? <Link to="/login">Sign in</Link>
+                    </p>
+                </form>
+            </div>
+        </div>
   )
 }
