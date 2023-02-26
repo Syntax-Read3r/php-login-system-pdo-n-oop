72549ad9419713910e116a9a2ea71f7e89d77f4f
Munyaradzi <syntaxread3r@gmail.com>
2023-02-26 15:49:31 +0000

    add temp val to ContextProvider.jsx useState and add logic to DefaultLayout.jsx

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,21 +1,44 @@
-import React from 'react'
-import { Navigate, Outlet } from 'react-router-dom'
-import { useStateContext } from '../contexts/ContextProvider'
+import React from "react";
+import { Link, Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function DefaultLayout() {
-  const {user, token} = useStateContext();
+    const { user, token } = useStateContext();
 
-  // check if the token not exist --  If it does not exist. The user does not have permission to access the page and is redirected to login page. 
-  // Anything that is part of the defaultLayout cannot be accessed if (!token)
-  if (!token) {
-    return <Navigate to="/login"/>
-  }
+    // check if the token not exist --  If it does not exist. The user does not have permission to access the page and is redirected to login page.
+    // Anything that is part of the defaultLayout cannot be accessed if (!token)
+    if (!token) {
+        return <Navigate to="/login" />;
+    }
 
+    const onLogout = (e) => {
+      return e.preventDefault();
+    }
 
-  return (
-    <div>
-      Default Layout
-      <Outlet/>
-    </div>
-  )
+    return (
+        <div id="defaultLayout">
+            <aside>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/users">Users</Link>
+            </aside>
+
+            <div className="content">
+                {/* HTML begins here */}
+                <header>
+                    <div>Header</div>
+
+                    <div>
+                        {user.name}
+                        <a href="#" onClick={onLogout} className="btn-logout">
+                            Logout
+                        </a>
+                    </div>
+                </header>
+
+                <main>
+                    <Outlet />
+                </main>
+            </div>
+        </div>
+    );
 }
--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -12,9 +12,9 @@ const StateContext = createContext(defaultValue);
 
 export const ContextProvider = ({children}) => {
     const [user, setUser] = useState({
-
+        name: 'Munya'
     });
-    const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+    const [token, _setToken] = useState(123);
 
     const setToken = (token) => {
         _setToken(token)
