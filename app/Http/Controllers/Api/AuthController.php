81bc01a488759dde8ebcd660778d04f7c294ae7b
Munyaradzi <syntaxread3r@gmail.com>
2023-02-26 21:55:06 +0000

    Start: create a controller Api/AuthController

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    //short cut to create a public fun is pubf
+    public function login() {
+
+    }
+
+    public function signup() {
+
+    }   
+    
+    public function logout() {
+
+    }
+}
