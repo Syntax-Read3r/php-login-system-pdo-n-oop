3c6c7aa76f8357cb5d7a584624f0fc5ad2839eb4
Munyaradzi <syntaxread3r@gmail.com>
2023-03-01 16:48:28 +0000

    display errors on signup screen when input field is left empty

--- react/src/pages/SignUp.jsx
+++ react/src/pages/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import axiosClient from "../axios-client";
 import { useStateContext } from "../contexts/ContextProvider";
@@ -8,31 +8,33 @@ export default function SignUp() {
     const emailRef = useRef();
     const passwordRef = useRef();
     const passwordConfirmationRef = useRef();
-
-    const {setUser, setToken} = useStateContext();
+    const [errors, setErrors] = useState();
+    const { setUser, setToken } = useStateContext();
 
     const onSubmit = (e) => {
         e.preventDefault();
 
         const payload = {
-          name: nameRef.current.value,
-          email: emailRef.current.value,
-          password: passwordRef.current.value,
-          password_confirmation: passwordConfirmationRef.current.value,
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
         };
-        axiosClient.post('/signup', payload)
-          .then(({data}) => {
-            setUser(data.user);
-            setToken(data.token);
-          })
-          .catch(error => {
-            const response = error.response;
+        axiosClient
+            .post("/signup", payload)
+            .then(({ data }) => {
+                setUser(data.user);
+                setToken(data.token);
+            })
+            .catch((error) => {
+                const response = error.response;
 
-            // 422 is a validation error
-            if (response && response.status === 422) {
-              console.log(response.data.errors);
-            }
-          })
+                // 422 is a validation error
+                if (response && response.status === 422) {
+                    console.log(response.data.errors);
+                    setErrors(response.data.errors);
+                }
+            });
     };
 
     return (
@@ -40,9 +42,28 @@ export default function SignUp() {
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Signup for free</h1>
+
+                    {/* display errors on screen if errors is true */}
+                    {/* errors to be displayed if error is set */}
+                    {errors && (
+                        <div className="alert">
+                            {Object.keys(errors).map((key) => (
+                                <p key={key}>{errors[key][0]}</p>
+                            ))}
+                        </div>
+                    )}
+
                     <input ref={nameRef} type="text" placeholder="Full Name" />
-                    <input ref={emailRef} type="email" placeholder="Email Address" />
-                    <input ref={passwordRef} type="password" placeholder="Password" />
+                    <input
+                        ref={emailRef}
+                        type="email"
+                        placeholder="Email Address"
+                    />
+                    <input
+                        ref={passwordRef}
+                        type="password"
+                        placeholder="Password"
+                    />
                     <input
                         ref={passwordConfirmationRef}
                         type="password"
